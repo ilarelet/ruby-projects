86666f917709a85d7da76c1451dc05c1c60672ff
Ilya Eletskiy <ilarelet@gmail.com>
2023-02-17 15:59:53 +0300

    Now the substrings are being counted with a hash

--- substring_count.rb
+++ substring_count.rb
@@ -1,14 +1,17 @@
 def substring_count(string, dictionary)
-    #dict_list = dictionary.split(",")
-    new_string = string.gsub("\"","")
+    #Creating a hash to store the final result
+    count_hash = Hash.new(0)
     dictionary.each do |word| 
-        stripped_word = word.gsub("\"","")
-        if new_string.include?(stripped_word) 
-            puts stripped_word
+        #Making sure the possible substring we are testing has no quotation marks and is downcase
+        stripped_word = word.gsub("\"","").downcase
+        if string.include?(stripped_word) 
+            #if the possible substring IS and actual substring of our string - add it to the hash
+            count_hash[stripped_word] +=1
         end
-
     end 
+    #returning the final result
+    count_hash
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substring_count('below', dictionary)
+puts substring_count('below', dictionary)
