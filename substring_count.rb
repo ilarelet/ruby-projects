0f7e6026fedd6176ed2ba21a7380dff37011ebf8
Ilya Eletskiy <ilarelet@gmail.com>
2023-02-20 11:35:33 +0300

    Words not used inthe input removed from the result

--- substring_count.rb
+++ substring_count.rb
@@ -2,8 +2,10 @@ def substring_count(inp_string, dictionary)
     #Making the search case insensitive
     string = inp_string.downcase
     dictionary.reduce(Hash.new(0)) do |count_hash, word|
+        if string.include?(word.downcase)
         #Count how many instances of the possible substring are present in the input
-        count_hash[word] = string.scan(word.downcase).length
+            count_hash[word] = string.scan(word.downcase).length
+        end
         #returning the final result
         count_hash               
     end
