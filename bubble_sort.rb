cbb3f7d31770356d4016d68ed15219eac378f717
Ilya Eletskiy <ilarelet@gmail.com>
2023-02-20 13:12:40 +0300

    Code is now sleeker and simplier

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,9 +5,7 @@ def bubble_sort(array)
         sorted = true
         array.each_index do |index|
             if array[index+1] and array[index]>array[index+1]
-                temp = array[index+1]
-                array[index+1] = array[index]
-                array[index] = temp
+                array[index + 1], array[index] = array[index], array[index + 1]
                 #if there's at least one case of two items being not in order
                 #the array is assumes not sorted, so the loop will run once again
                 sorted=false
