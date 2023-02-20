15bbe2b87776dc84c7210d0a0e8c628707c004b9
Ilya Eletskiy <ilarelet@gmail.com>
2023-02-20 11:29:36 +0300

    Code otimized using enumerable methods

--- substring_count.rb
+++ substring_count.rb
@@ -1,22 +1,12 @@
 def substring_count(inp_string, dictionary)
-    #Creating an array of strings out of the input
-    strings = inp_string.downcase.split(" ")
-
-    #Creating a hash to store the final result
-    count_hash = Hash.new(0)
-    dictionary.each do |word| 
-        #checking each of words in the iputted string
-        strings.each do |string|
-            #Making sure the possible substring we are testing has no quotation marks and is downcase
-            stripped_word = word.gsub("\"","").downcase
-            if string.include?(stripped_word) 
-                #if the possible substring IS and actual substring of our string - add it to the hash
-                count_hash[stripped_word] +=1
-            end
-        end
-    end 
-    #returning the final result
-    count_hash
+    #Making the search case insensitive
+    string = inp_string.downcase
+    dictionary.reduce(Hash.new(0)) do |count_hash, word|
+        #Count how many instances of the possible substring are present in the input
+        count_hash[word] = string.scan(word.downcase).length
+        #returning the final result
+        count_hash               
+    end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
