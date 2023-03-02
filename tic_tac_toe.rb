6709b9b6d1f8808b995e20b67f4a12fdeaae4627
Ilya Eletskiy <ilarelet@gmail.com>
2023-03-02 17:19:45 +0300

    Creating the field of 9 cells

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,25 @@
 class Cell
-    attr_read: row, column
-    attr_accessor: tick_tack
+    attr_reader :row, :column
+    attr_accessor :sign
     
     def initialize(row, column)
         @row = row
         @column = column
-        @tick_tack = nil
+        @sign = nil
     end
-
+    #Marking a cell with a sign (X or 0)
     def mark(sign)
-        @tick_tack = sign
+        @sign = sign
     end
 end
 
+#Creating the two-dimensional array
+rows = 0..2
+columns = 0..2
+field = Array.new(3){Array.new(3)}
+#Filling the array with Cells
+rows.each do |row| 
+    columns.each do |column|
+        field[row][column] = Cell.new(row, column)
+    end
+end
